afbbbe949e9a08254ad4f2db9b9538014bdc42c5
darvishdarab <mali_dd@yahoo.com>
2024-02-12 10:38:07 -0500

    added ex10

---
+++ exercises/ex10/passing.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+// TODO: declare the getDate function
+
+int main() {
+
+  int mon = 0;
+  int day = 0;
+  int yr = 0;
+
+  // TODO: declare and initialize an array of strings that are the names of the
+  // months of the year. You can use their 3-character abbreviations, such as "Feb".
+  
+  while (  /* TODO: call the getDate function */ == 3) {
+    printf("%s %d, %d\n", months[mon-1], day, yr);
+  }
+  
+  return 0;
+}
+
+// TODO: define the getDate function to prompt for and read a date in the form
+// MM/DD/YYYY, passing the inputs as integers back to main through the parameters
+// and returning the number of values successfully read
+
+
